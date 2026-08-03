81acabd778a90b35f539bd4eb2df10b85bf6f274
melvinquick <melvinquick@proton.me>
2026-08-03 15:35:19 -0400

    Cleaned up how character pools are selected to get rid of the headache of a match string.

--- src/psswd_box/password_generator.py
+++ src/psswd_box/password_generator.py
@@ -26,60 +26,51 @@ class PasswordGenerator:
             "+",
         ]
 
-    def generate_password(
-        self, character_types=["y", "y", "y", "y"], num_characters=20
-    ):
-        if character_types == ["n", "n", "n", "n"]:
-            print("You didn't include any character types... Exiting")
-            return exit()
+    def generate_password(self, character_types=None, num_characters=24):
+        if character_types is None:
+            character_types = ["y", "y", "y", "y"]
 
-        choices = self.get_valid_choices(character_types)
-        character_counter = 0
-        psswd = ""
+        pools = self.get_pools(character_types)
 
-        while character_counter < num_characters:
-            psswd += choice(choices)
-            character_counter += 1
+        if not pools or num_characters <= 0:
+            return ""
 
-        return psswd
+        password_chars = []
+
+        for pool in pools:
+            if len(password_chars) < num_characters:
+                password_chars.append(choice(pool))
+
+        all_choices = [char for pool in pools for char in pool]
+
+        while len(password_chars) < num_characters:
+            password_chars.append(choice(all_choices))
+
+        self.rng.shuffle(password_chars)
+
+        return "".join(password_chars[:num_characters])
+
+    def get_pools(self, character_types):
+        pools = []
+
+        if len(character_types) > 0 and character_types[0] == "y":
+            pools.append(self.lowercase_letters)
+
+        if len(character_types) > 1 and character_types[1] == "y":
+            pools.append(self.uppercase_letters)
+
+        if len(character_types) > 2 and character_types[2] == "y":
+            pools.append(self.numbers)
+
+        if len(character_types) > 3 and character_types[3] == "y":
+            pools.append(self.symbols)
+
+        return pools
 
     def get_valid_choices(self, character_types):
-        match character_types:
-            case ["y", "y", "y", "y"]:
-                return (
-                    self.lowercase_letters
-                    + self.uppercase_letters
-                    + self.numbers
-                    + self.symbols
-                )
-            case ["y", "y", "y", "n"]:
-                return self.lowercase_letters + self.uppercase_letters + self.numbers
-            case ["y", "y", "n", "n"]:
-                return self.lowercase_letters + self.uppercase_letters
-            case ["y", "n", "n", "n"]:
-                return self.lowercase_letters
-            case ["y", "n", "y", "y"]:
-                return self.lowercase_letters + self.numbers + self.symbols
-            case ["y", "n", "y", "n"]:
-                return self.lowercase_letters + self.numbers
-            case ["y", "n", "n", "y"]:
-                return self.lowercase_letters + self.symbols
-            case ["y", "y", "n", "y"]:
-                return self.lowercase_letters + self.uppercase_letters + self.symbols
-            case ["n", "y", "y", "y"]:
-                return self.uppercase_letters + self.numbers + self.symbols
-            case ["n", "n", "y", "y"]:
-                return self.numbers + self.symbols
-            case ["n", "n", "n", "y"]:
-                return self.symbols
-            case ["n", "y", "n", "y"]:
-                return self.uppercase_letters + self.symbols
-            case ["n", "y", "n", "n"]:
-                return self.uppercase_letters
-            case ["n", "n", "y", "n"]:
-                return self.numbers
-            case ["n", "y", "y", "n"]:
-                return self.uppercase_letters + self.numbers
-            case ["n", "n", "n", "n"]:
-                print("You didn't include any character types... Exiting")
-                return exit()
+        choices = []
+
+        for pool in self.get_pools(character_types):
+            choices.extend(pool)
+
+        return choices
